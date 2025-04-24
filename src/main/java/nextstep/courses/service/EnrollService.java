eafc1aef08d1f8bae4137374a7c4ece6ed7a44d0
김동환 <kimds989@naver.com>
2025-04-24 12:46:12 +0900

    refactor(step3): Refactor EnrollService to handle Participant logic

--- src/main/java/nextstep/courses/service/EnrollService.java
+++ src/main/java/nextstep/courses/service/EnrollService.java
@@ -1,19 +1,33 @@
 package nextstep.courses.service;
 
+import nextstep.courses.domain.Participant;
+import nextstep.courses.domain.ParticipantRepository;
+import nextstep.courses.domain.Participants;
+import nextstep.courses.domain.Session;
 import nextstep.courses.domain.SessionRepository;
 import nextstep.payments.domain.Payment;
+import org.springframework.transaction.annotation.Transactional;
 
 public class EnrollService {
 
     private final SessionRepository sessionRepository;
 
-    public EnrollService(SessionRepository sessionRepository) {
+    private final ParticipantRepository participantRepository;
+
+    public EnrollService(SessionRepository sessionRepository, ParticipantRepository participantRepository) {
         this.sessionRepository = sessionRepository;
+        this.participantRepository = participantRepository;
     }
 
+    @Transactional
     public void enroll(Long sessionId, Long userId, Payment payment) {
-        sessionRepository.findById(sessionId)
-                .orElseThrow(() -> new IllegalArgumentException("Session not found"))
-                .enroll(userId, payment);
+        Session session = sessionRepository.findById(sessionId)
+                .orElseThrow(() -> new IllegalArgumentException("Session not found"));
+
+        // service param에는 안 넣음, 조회
+        Participants participants = participantRepository.findBySessionId(sessionId);
+
+        Participant enrolledParticipant = session.enroll(userId, payment, participants);
+        participantRepository.save(sessionId, enrolledParticipant);
     }
 }
